61cdd27706042623c6523472d401f0cc96defa65
Adi Salimgereev <adisalimgereev@gmail.com>
2023-08-17 23:06:44 +0600

    fix: convert tabs to spaces

--- apple_map.c
+++ apple_map.c
@@ -6,10 +6,10 @@
 typedef struct bucket bucket;
 
 /**
- * @brief			Hashmap is a data structure, that maps keys to values. Values in the
- * 						apple map implementation are pointer values or integral types.
+ * @brief      Hashmap is a data structure, that maps keys to values. Values in the
+ *             apple map implementation are pointer values or integral types.
  *
- * @version		0.1.0
+ * @version    0.1.0
  */
 struct apple_map
 {
@@ -41,10 +41,10 @@ static bucket *resize_entry(apple_map *map, bucket *entry);
 const size_t DEFAULT_CAPACITY = 30;
 
 /**
- * @brief			Creates a new empty hashmap.
- * @returns		A newly allocated empty hashmap.
+ * @brief      Creates a new empty hashmap.
+ * @returns    A newly allocated empty hashmap.
  *
- * @version 	0.1.0
+ * @version    0.1.0
  */
 apple_map *apple_map_new(void)
 {
@@ -67,12 +67,12 @@ apple_map *apple_map_new(void)
 }
 
 /**
- * @brief							Frees the hashmap object and inner buckets.
- * @details						This function doesn't free the key-value pairs. You can achieve that by
- * 										using `apple_map_iter` with callback freeing the hashmap entries.
- * @param map					The hashmap object to free.
+ * @brief              Frees the hashmap object and inner buckets.
+ * @details            This function doesn't free the key-value pairs. You can achieve that by
+ *                     using `apple_map_iter` with callback freeing the hashmap entries.
+ * @param map          The hashmap object to free.
  *
- * @version						0.1.0
+ * @version            0.1.0
  */
 inline void apple_map_free(apple_map *map)
 {
@@ -81,16 +81,16 @@ inline void apple_map_free(apple_map *map)
 }
 
 /**
- * @brief							Resolves a key-value pair from the hashmap.
+ * @brief              Resolves a key-value pair from the hashmap.
  *
- * @param map					The hashmap, from which the key-value pair will be resolved.
- * @param key					The key to resolve.
- * @param key_size		The size of the key.
- * @param out_value		The reference to a value to store the resolved value.
+ * @param map          The hashmap, from which the key-value pair will be resolved.
+ * @param key          The key to resolve.
+ * @param key_size     The size of the key.
+ * @param out_value    The reference to a value to store the resolved value.
  *
- * @returns						The resolved value.
+ * @returns            The resolved value.
  *
- * @version						0.1.0
+ * @version            0.1.0
  */
 bool apple_map_get(apple_map *map, const void *key, size_t key_size, uintptr_t *out_value)
 {
@@ -168,15 +168,15 @@ static inline uint32_t fnv_1a_hash(const unsigned char *data, size_t size)
 const float MAX_CAPACITY_PERCENTAGE = 0.75;
 
 /**
- * @brief						Inserts a key-value pair into the hashmap.
- * @details					Function doesn't copy a key, so you should guarantee its lifetime.
+ * @brief            Inserts a key-value pair into the hashmap.
+ * @details          Function doesn't copy a key, so you should guarantee its lifetime.
  *
- * @param map  			The hashmap, into which the key-value pair will be inserted.
- * @param key  			The key, to insert into the hashmap.
- * @param key_size	The size of the key.
- * @param value 		The value, to insert into the hashmap.
+ * @param map        The hashmap, into which the key-value pair will be inserted.
+ * @param key        The key, to insert into the hashmap.
+ * @param key_size   The size of the key.
+ * @param value      The value, to insert into the hashmap.
  *
- * @version					0.1.0
+ * @version          0.1.0
  */
 void apple_map_insert(apple_map *map, const void *key, size_t key_size, uintptr_t value)
 {
@@ -207,20 +207,20 @@ void apple_map_insert(apple_map *map, const void *key, size_t key_size, uintptr_
 }
 
 /**
- * @brief							Tries to resolve a key-value pair from the hashmap. If it fails, it adds the
- * 										entry into the hashmap, with the value read from `out_in`. If it doesn't, `out_in`
- * 										will be set to the value from the hashmap entry successfully resolved by key.
+ * @brief              Tries to resolve a key-value pair from the hashmap. If it fails, it adds the
+ *                     entry into the hashmap, with the value read from `out_in`. If it doesn't, `out_in`
+ *                     will be set to the value from the hashmap entry successfully resolved by key.
  *
- * @param map					The hashmap, from which the key-value pair will be resolved.
- * @param key					The key to resolve.
- * @param key_size		The size of the key.
- * @param out_in			The reference to a value to store it in a new hashmap entry or the
- * 										value that will be set to the value of successfully resolved entry.
+ * @param map          The hashmap, from which the key-value pair will be resolved.
+ * @param key          The key to resolve.
+ * @param key_size     The size of the key.
+ * @param out_in       The reference to a value to store it in a new hashmap entry or the
+ *                     value that will be set to the value of successfully resolved entry.
  *
- * @returns						`true` if the key-value pair already exists.
- * 										`false` otherwise.
+ * @returns            `true` if the key-value pair already exists.
+ *                     `false` otherwise.
  *
- * @version						0.1.0
+ * @version            0.1.0
  */
 bool apple_map_get_or_insert(apple_map *map, const void *key, size_t key_size, uintptr_t *out_in)
 {
@@ -254,12 +254,12 @@ bool apple_map_get_or_insert(apple_map *map, const void *key, size_t key_size, u
 }
 
 /**
- * @brief						Removes a key-value pair resolved by key from the hashmap.
- * @param map				The hashmap, from which the key-value pair will be removed.
- * @param	key				The key, via which entry is resolved.
- * @param	key_size	The size of the key.
+ * @brief            Removes a key-value pair resolved by key from the hashmap.
+ * @param map        The hashmap, from which the key-value pair will be removed.
+ * @param  key       The key, via which entry is resolved.
+ * @param  key_size  The size of the key.
  *
- * @version					0.1.0
+ * @version          0.1.0
  */
 void apple_map_remove(apple_map *map, const void *key, size_t key_size)
 {
@@ -276,15 +276,15 @@ void apple_map_remove(apple_map *map, const void *key, size_t key_size)
 }
 
 /**
- * @brief							Removes a key-value pair resolved by key from the hashmap. But unlike
- * 										`apple_map_remove`, it will free the key-value pair's data via the `callback`.
- * @param map					The hashmap, from which the key-value pair will be removed.
- * @param	key					The key, via which entry is resolved.
- * @param	key_size		The size of the key.
- * @param	callback		The callback, that will be called to free the key-value pair.
- * @param	user				User pointer is a pointer that you can use in the `callback`.
+ * @brief              Removes a key-value pair resolved by key from the hashmap. But unlike
+ *                     `apple_map_remove`, it will free the key-value pair's data via the `callback`.
+ * @param map          The hashmap, from which the key-value pair will be removed.
+ * @param  key         The key, via which entry is resolved.
+ * @param  key_size    The size of the key.
+ * @param  callback    The callback, that will be called to free the key-value pair.
+ * @param  user        User pointer is a pointer that you can use in the `callback`.
  *
- * @version						0.1.0
+ * @version            0.1.0
  */
 void apple_map_remove_free(apple_map *map, const void *key, size_t key_size,
                            apple_map_callback callback, void *user)
@@ -304,16 +304,16 @@ void apple_map_remove_free(apple_map *map, const void *key, size_t key_size,
 }
 
 /**
- * @brief							Similiar to `apple_map_insert`, but when trying to overwrite a hashmap entry,
- * 										it will free the old entry's data via callback using `callback`.
- * @param map					The hashmap, into which the key-value pair will be soft-inserted.
- * @param key					The key, to insert into the hashmap.
- * @param key_size		The size of the key.
- * @param value 			The value, to insert into the hashmap.
- * @param callback		The callback, that will be called when the key-value pair already exists.
- * @param user				User pointer is a pointer that you can use in the `callback`.
+ * @brief              Similiar to `apple_map_insert`, but when trying to overwrite a hashmap entry,
+ *                     it will free the old entry's data via callback using `callback`.
+ * @param map          The hashmap, into which the key-value pair will be soft-inserted.
+ * @param key          The key, to insert into the hashmap.
+ * @param key_size     The size of the key.
+ * @param value        The value, to insert into the hashmap.
+ * @param callback     The callback, that will be called when the key-value pair already exists.
+ * @param user         User pointer is a pointer that you can use in the `callback`.
  *
- * @version						0.1.0
+ * @version            0.1.0
  */
 void apple_map_soft_insert(apple_map *map, const void *key, size_t key_size,
                            uintptr_t value, apple_map_callback callback, void *user)
@@ -349,10 +349,10 @@ void apple_map_soft_insert(apple_map *map, const void *key, size_t key_size,
 }
 
 /**
- * @brief						Returns the number of entries in the hashmap.
- * @returns					The number of entries in the hashmap.
+ * @brief            Returns the number of entries in the hashmap.
+ * @returns          The number of entries in the hashmap.
  *
- * @version					0.1.0
+ * @version          0.1.0
  */
 inline size_t apple_map_len(apple_map *map)
 {
@@ -362,10 +362,10 @@ inline size_t apple_map_len(apple_map *map)
 const float RESIZE_FACTOR_PERCENTAGE = 2;
 
 /**
- * @brief							Resizes the hashmap to a new capacity when the hashmap is full.
- * @param map					The hashmap to iterate.
+ * @brief              Resizes the hashmap to a new capacity when the hashmap is full.
+ * @param map          The hashmap to iterate.
  *
- * @version						0.1.0
+ * @version            0.1.0
  */
 void apple_map_resize(apple_map *map)
 {
@@ -415,12 +415,12 @@ static bucket *resize_entry(apple_map *map, bucket *entry)
 }
 
 /**
- * @brief							Iterates through the hashmap, using the `callback`.
- * @param map					The hashmap to iterate.
- * @param callback		The callback, that will be called on each entry.
- * @param user				User pointer is a pointer that you can use in the `callback`.
+ * @brief              Iterates through the hashmap, using the `callback`.
+ * @param map          The hashmap to iterate.
+ * @param callback     The callback, that will be called on each entry.
+ * @param user         User pointer is a pointer that you can use in the `callback`.
  *
- * @version						0.2.0
+ * @version            0.2.0
  */
 void apple_map_iter(apple_map *map, apple_map_callback callback, void *user)
 {
--- examples/insert_and_get.c
+++ examples/insert_and_get.c
@@ -3,43 +3,43 @@
 
 int main()
 {
-	apple_map *map = apple_map_new();
+  apple_map *map = apple_map_new();
 
-	printf("map.len() = %ld\n", apple_map_len(map));
+  printf("map.len() = %ld\n", apple_map_len(map));
 
-	apple_map_insert(map, "hello", sizeof("hello") - 1, 1);
-	apple_map_insert(map, "world", sizeof("world") - 1, 2);
+  apple_map_insert(map, "hello", sizeof("hello") - 1, 1);
+  apple_map_insert(map, "world", sizeof("world") - 1, 2);
 
-	printf("map.len() = %ld\n", apple_map_len(map));
+  printf("map.len() = %ld\n", apple_map_len(map));
 
-	uintptr_t expected_value;
+  uintptr_t expected_value;
 
-	if (apple_map_get(map, "hello", sizeof("hello") - 1, &expected_value))
-	{
-		printf("map[\"hello\"] = %ld\n", expected_value);
-	}
-	else
-	{
-		printf("map[\"hello\"] = undefined\n");
-	}
+  if (apple_map_get(map, "hello", sizeof("hello") - 1, &expected_value))
+  {
+    printf("map[\"hello\"] = %ld\n", expected_value);
+  }
+  else
+  {
+    printf("map[\"hello\"] = undefined\n");
+  }
 
-	if (apple_map_get(map, "world", sizeof("world") - 1, &expected_value))
-	{
-		printf("map[\"world\"] = %ld\n", expected_value);
-	}
-	else
-	{
-		printf("map[\"world\"] = undefined\n");
-	}
+  if (apple_map_get(map, "world", sizeof("world") - 1, &expected_value))
+  {
+    printf("map[\"world\"] = %ld\n", expected_value);
+  }
+  else
+  {
+    printf("map[\"world\"] = undefined\n");
+  }
 
-	if (apple_map_get(map, "!", sizeof("!") - 1, &expected_value))
-	{
-		printf("map[\"!\"] = %ld\n", expected_value);
-	}
-	else
-	{
-		printf("map[\"!\"] = undefined\n");
-	}
+  if (apple_map_get(map, "!", sizeof("!") - 1, &expected_value))
+  {
+    printf("map[\"!\"] = %ld\n", expected_value);
+  }
+  else
+  {
+    printf("map[\"!\"] = undefined\n");
+  }
 
-	apple_map_free(map);
+  apple_map_free(map);
 }
